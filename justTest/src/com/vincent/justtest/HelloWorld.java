0ebf27ec32596757d3254cc54b7b82ec22b6de15
vincent <liutongqing21@163.com>
2017-09-10 22:49:09 +0800

    modify HelloWorld.java

--- justTest/src/com/vincent/justtest/HelloWorld.java
+++ justTest/src/com/vincent/justtest/HelloWorld.java
@@ -3,5 +3,6 @@ package com.vincent.justtest;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("hello vincent!!!!");
 	}
 }
