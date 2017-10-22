dcb83957dda5e2ec88d93d261235c7dc13d92308
vincent <liutongqing21@163.com>
2017-10-22 23:15:28 +0800

    update testTestNG

---
+++ testTestNG/src/com/vincent/testng/TestRuntime.java
@@ -0,0 +1,22 @@
+/**   
+* @Title: TestRuntime.java 
+* @Package com.vincent.testng 
+* @Description: TODO
+* @author Vincent  
+* @date 2017年10月22日 下午11:08:23 
+* @version V1.0   
+*/ 
+package com.vincent.testng;
+
+import org.testng.annotations.Test;
+/**
+ * @author Vincent
+ *
+ */
+public class TestRuntime {
+	@Test(expectedExceptions = ArithmeticException.class)
+	public void divisionWithException() {
+		int i = 1 / 0;
+		System.out.println("After division the value of i is: "+i);
+	}
+}
