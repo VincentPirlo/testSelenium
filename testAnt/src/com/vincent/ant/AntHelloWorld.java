54396164223f3362043e8656a65a2325794a4e2c
vincent <liutongqing21@163.com>
2017-10-24 23:04:33 +0800

    update testAnt Projectt

---
+++ testAnt/src/com/vincent/ant/AntHelloWorld.java
@@ -0,0 +1,19 @@
+/**   
+* @Title: AntHelloWorld.java 
+* @Package com.vincent.ant 
+* @Description: TODO
+* @author Vincent  
+* @date 2017年10月24日 下午11:01:51 
+* @version V1.0   
+*/ 
+package com.vincent.ant;
+
+/**
+ * @author Vincent
+ *
+ */
+public class AntHelloWorld { 
+    public static void main(String[] args) { 
+        System.out.println("Hello World"); 
+    } 
+} 
