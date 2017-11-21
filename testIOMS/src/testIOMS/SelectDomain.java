3d23d66adb1c44a85de515dee814571d97c3c267
vincent <liutongqing21@163.com>
2017-11-21 23:01:02 +0800

    selenium cssselector locate

--- testIOMS/src/testIOMS/SelectDomain.java
+++ testIOMS/src/testIOMS/SelectDomain.java
@@ -1,27 +1,28 @@
 package testIOMS;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-
 public class SelectDomain {
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver","C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://10.10.40.50");
-		
+
 		driver.findElement(By.id("real_username")).sendKeys("admin");
 		driver.findElement(By.id("fake_pass")).click();
 		driver.findElement(By.id("real_pass")).sendKeys("admin888");
 		driver.findElement(By.id("submit")).click();
-		
+
 		driver.findElement(By.id("dmu_device_addDevBtn")).click();
 		Select sel = new Select(driver.findElement(By.id("dmu_device_winadddev_domainCombo")));
-		for(WebElement e:sel.getOptions()) {
+		for (WebElement e : sel.getOptions()) {
 			String a = "°²»Õ";
-			if(a.equals(e.getText())) {
+			if (a.equals(e.getText())) {
 				e.click();
 			}
 		}
---
+++ testSelenium/src/com/vincent/selenium/CssSelector.java
@@ -0,0 +1,49 @@
+/**   
+* @Title: CssSelector.java 
+* @Package com.vincent.selenium 
+* @Description: TODO
+* @author Vincent  
+* @date 2017年11月21日 下午9:37:26 
+* @version V1.0   
+*/ 
+package com.vincent.selenium;
+
+import java.io.File;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.*;
+
+
+/**
+ * @Function: TODO
+ * @author: Vincent
+ * @date: 2017年11月21日下午9:37:26
+ */
+public class CssSelector {
+	@Test
+	public void csstest() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		//driver.get("https://www.baidu.com");
+		//driver.manage().window().maximize();
+		
+		//driver.findElement(By.cssSelector("#kw")).sendKeys("selenium");
+		//driver.findElement(By.cssSelector("#su")).click();
+		
+		
+		File file = new File("D:\\test-workspace\\testSelenium\\src\\css.html");
+		String filepath = file.getAbsolutePath();
+		driver.get("file:///"+filepath);
+		Thread.sleep(2000);
+		
+		WebElement ele1 = driver.findElement(By.xpath("id('index')/div[2]/div/ul/div/li[2]/a"));
+		WebElement ele2 = driver.findElement(By.cssSelector("#index div:nth-child(2) div ul div li:nth-child(2) a"));
+		Assert.assertEquals(ele1.getText(), ele2.getText());
+		driver.quit();
+	}
+
+}
