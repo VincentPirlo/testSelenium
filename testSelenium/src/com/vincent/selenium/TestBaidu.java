b1b255df2e9359530e3eabfea5216eb26e8dd5f8
vincent <liutongqing21@163.com>
2018-11-19 21:01:13 +0800

    update

--- testSelenium/src/com/vincent/selenium/TestBaidu.java
+++ testSelenium/src/com/vincent/selenium/TestBaidu.java
@@ -2,7 +2,7 @@ package com.vincent.selenium;
 
 import org.openqa.selenium.WebDriver;
 //import org.openqa.selenium.firefox.*;
-//import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.ie.*;
 //import org.openqa.selenium.edge.*;
 
@@ -12,21 +12,22 @@ import org.openqa.selenium.ie.*;
  * @date: 2017年12月18日下午9:30:01
  */
 public class TestBaidu {
-public static void main(String[] args) {
+public static void main(String[] args) throws InterruptedException {
 	//System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\Mozilla Firefox\\geckodriver.exe");
-	//System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");
-	System.setProperty("webdriver.ie.driver","C:\\Program Files (x86)\\Internet Explorer\\IEDriverServer.exe");
+	System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");
+	//System.setProperty("webdriver.ie.driver","C:\\Program Files (x86)\\Internet Explorer\\IEDriverServer.exe");
 	//System.setProperty("webdriver.edge.driver", "C:\\Program Files (x86)\\Internet Explorer\\MicrosoftWebDriver.exe");
 	
 	System.out.println("seting brwoser");
 	
 	//WebDriver driver = new FirefoxDriver();
-	//WebDriver driver = new ChromeDriver();
-	WebDriver driver = new InternetExplorerDriver();
+	WebDriver driver = new ChromeDriver();
+	//WebDriver driver = new InternetExplorerDriver();
 	//WebDriver driver = new EdgeDriver();
 	
-	
+	Thread.sleep(1000);
 	driver.manage().window().maximize();
+	Thread.sleep(1000);
 	driver.get("https://www.baidu.com");
 	String title = driver.getTitle();
 	System.out.println(title);
